e34d835cab97e153e3bb024fcf1e49a3caccb1d5
DariusVArts <odvalenc@gmail.com>
2025-12-17 21:04:34 -0500

    Textures Update

--- Assets/Props_ArtTeam/08 Technical Artist/Anim.cs
+++ Assets/Props_ArtTeam/08 Technical Artist/Anim.cs
@@ -1,7 +1,22 @@
 using UnityEngine;
+using static ControlAnimation;
 
 public class Anim : MonoBehaviour
 {
+    public Animations animations;
+
+    public enum Animations
+    {
+        Run,
+        Headbust,
+        BrokenGlass,
+        PunchesWindow,
+        EnterWindow,
+        HandAttack,
+        Die01,
+        Die02
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
